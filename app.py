e7397fa2e5df39fa1e8c9e594edb270902830c4c
Daniel <danielbetzalel16@gmail.com>
2024-04-16 17:17:44 +0300

    POST endpoints & added HTML pages (templates)

--- app.py
+++ app.py
@@ -3,16 +3,18 @@ from flask import Flask, flash, jsonify, redirect, render_template, request, url
 import sys
 from simplegmail import Gmail
 from simplegmail.query import construct_query
+import logging
 
 app = Flask(__name__)
 app.secret_key = "secret"
 gmail = Gmail()
 sys.stdout.reconfigure(encoding='utf-8')
+logging.basicConfig(level=logging.INFO)
 
 
 @app.route("/")
 def home():
-    return get_messages_by_label("IMPORTANT")
+    return redirect(url_for("user", usr="Daniel", messages=[]))
 
 
 @app.route("/api/v1/gmail/login", methods=["POST", "GET"])
@@ -25,17 +27,20 @@ def logout():
     return redirect(url_for("login"))
 
 
-@app.route("/api/v1/gmail/user")
-def user(usr):
-    messages = get_messages()
-    return render_template("user.html", name=usr, messages=messages)
+@app.route("/api/v1/gmail/user/<string:usr>/<messages>")
+def user(usr, messages):
+    labels = gmail.list_labels()  # get all the labels the current user have
+    if messages == "[]":
+        messages = get_messages()
+
+    return render_template("user.html", messages=messages, name=usr, labels=labels)
 
 
 @app.route('/api/v1/gmail/messages', methods=['GET'])
 def get_messages():
     try:
         messages = gmail.get_messages()
-        return render_template("user.html", messages=messages, name="Daniel")
+        return messages
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
@@ -72,6 +77,66 @@ def get_messages_by_label(wanted_label):
     try:
         messages = gmail.get_messages(query=f"label: {wanted_label}")
         return render_template("user.html", messages=messages, name="Daniel")
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/api/v1/gmail/messages/send', methods=['POST'])
+def send_message():
+    try:
+        to = request.form['to']
+        subject = request.form['subject']
+        message = request.form['message']
+        data = {
+            "to": to,
+            "sender": "danielbetzalel16@gmail.com",
+            "subject": subject,
+            "msg_html": message,
+            "msg_plain": message,
+            "signature": True
+        }
+        sent = gmail.send_message(**data)
+        redirect("user")
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/api/v1/gmail/messages/move_to_garbage', methods=['POST'])
+def move_to_garbage(message):
+    try:
+        if not message:
+         desired_message_id = request.form['message_id']
+        else:
+            desired_message_id = message.id
+        messages = gmail.get_messages()
+        for message in messages:
+            if message.id == desired_message_id:
+                the_msg = message
+        # every msg will be "read" if it goes to the trash
+        the_msg.mark_as_read()
+        the_msg.trash()
+        return redirect(url_for("user", usr="Daniel", messages=[]))
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/api/v1/gmail/messages/change_label', methods=['POST'])
+def change_label():
+    try:
+        desired_message_id = request.form['message_id']
+        desired_label = request.form['wanted_label']
+
+        messages = gmail.get_messages()
+        for message in messages:
+            if message.id == desired_message_id:
+                the_msg = message
+                
+        if desired_label == "TRASH":
+            move_to_garbage(message=the_msg)
+        
+        the_msg.add_label(desired_label)
+        return redirect(url_for("user", usr="Daniel", messages=[]))
+
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
